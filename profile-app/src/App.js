ee43839b08e6bf0a675a2afb8879d40d7141b4db
Chris Lopez <clopez12@emich.edu>
2021-09-29 21:55:34 +0000

    Add some styles

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Footer from './Components/Footer';
-import Education from './Components/About'
-import { Languages } from './Components/About';
+import { Languages, Education } from './Components/About';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Programmer Profile</h1>
       <h2>I'm a student and Junior Web Developer</h2>
       <Education />
---
+++ profile-app/src/Components/Nav.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Nav() {
+  return (
+    <div class="Nav">
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+  );
+}
+
+export default Nav;
